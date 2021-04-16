c26b1541a4b5b9632fca253ed1c8a37f58a39985
tylerwhyler <tyjiman@gmail.com>
2021-04-16 05:11:21 -0700

    appleAndOrange problem took about 2 hours of dumb bug troubleshooting. I forgot how to declare a variable in a for loop... for example

--- appleAndOrange.js
+++ appleAndOrange.js
@@ -1,3 +1,17 @@
-//test. Linux/VScodium are a bitch to get to work with github wow
+//easy hackerrank problem; "Apple and Orange"
 
-//second test
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleCount = 0;
+    let orangeCount = 0;
+    for (let x = 0; x <= apples.length; x++) {
+        if ((a + apples[x]) >= s && (a + apples[x]) <= t) {
+            appleCount++;
+        }
+    }
+    for (let x = 0; x <= oranges.length; x++) {
+        if ((b + oranges[x]) <= t && (b + oranges[x]) >= s) {
+            orangeCount++;
+        }
+    }
+    console.log(String(`${appleCount}\n${orangeCount}`))
+}
